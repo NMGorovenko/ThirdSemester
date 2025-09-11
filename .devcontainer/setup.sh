6c6a8431787de76eff515777fb73359e503d0d1e
Nikita Gorovenko <nmgorovenko@yandex.ru>
2025-09-11 17:18:47 +0700

    Add setup script for devcontainer configuration

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# Базовые пакеты
+sudo apt-get update
+sudo apt-get install -y git-lfs
+
+# Настройка Git LFS
+git lfs install
+
+# Python пакеты
+pip install --upgrade pip
+pip install jupyterlab nbconvert
+
+# .NET Interactive
+dotnet tool install -g Microsoft.dotnet-interactive || dotnet tool update -g Microsoft.dotnet-interactive
+/home/vscode/.dotnet/tools/dotnet-interactive jupyter install --default-kernel csharp
