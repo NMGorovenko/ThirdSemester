f8eb3b18cd60b8734a124ae995e8a36c7b96606e
Nikita Gorovenko <nmgorovenko@yandex.ru>
2025-09-11 18:58:13 +0700

    Add Git LFS setup to postStartCommand and clean up setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 set -e
 
-# Базовые пакеты
+# Устанавливаем Git LFS
 sudo apt-get update
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt-get install -y git-lfs
 
-# Настройка Git LFS
-git lfs install
-git lfs pull
-
-# Настройка Git LFS
-git lfs install
-git lfs pull
-
-# Python пакеты
+# Настройка Python
 pip install --upgrade pip
 pip install jupyterlab nbconvert
 
-# .NET Interactive
+# Устанавливаем .NET Interactive
 dotnet tool install -g Microsoft.dotnet-interactive || dotnet tool update -g Microsoft.dotnet-interactive
 /home/vscode/.dotnet/tools/dotnet-interactive jupyter install --default-kernel csharp
